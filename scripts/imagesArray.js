fe295f2afdde5603c228cb4f8a2b528cababc5af
krasotun <servrostov@gmail.com>
2022-02-22 00:52:42 +0300

    add Добавил массив с картинками

---
+++ scripts/imagesArray.js
@@ -0,0 +1,11 @@
+const imagesArray = [{
+    name: 'Winter',
+    link: 'img/portfolio/portfolio-img-1.jpg',
+    alt: 'Nice Winter Image from portfolio'
+  },
+  {
+    name: 'Spring',
+    link: 'img/portfolio/portfolio-img-2.jpg',
+    alt: 'Nice Spring Image from portfolio'
+  }
+];
