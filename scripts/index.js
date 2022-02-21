6ac5e95258dcdf5015a1db6d7721452fccefac1b
Marat <servrostov@gmail.com>
2022-02-21 17:38:19 +0300

    add Реализовал функцию табов

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,36 @@
 // Работа с табами
+
+// Выбираем контейнер с табами
+const tabsContainer = document.querySelector('.portfolio__tabs-wrapper');
+
+// Выбираем табы в контейнере
+const portfolioTabs = tabsContainer.querySelectorAll('.tab');
+console.log(portfolioTabs);
+
+// Выбираем картинки из портфолио по клику
+
+const selectImagesFromPortfolio = function () {
+  tabsContainer.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('tab')) {
+      let clickedTag = evt.target;
+      removeActiveTabs();
+      togglePortfolioTag(clickedTag);
+    }
+  });
+};
+
+selectImagesFromPortfolio();
+
+// Для смены класса у кликнутого таба
+const togglePortfolioTag = function (clickedTag) {
+  clickedTag.classList.toggle('tab_active');
+};
+
+// Убирает active у кого он есть
+const removeActiveTabs = function () {
+  portfolioTabs.forEach(tab => {
+    if (tab.classList.contains('tab_active')) {
+      tab.classList.remove('tab_active');
+    }
+  });
+};
