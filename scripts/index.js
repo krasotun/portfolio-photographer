1b715217683b8b49eed5e14cb46e41170d039bb1
krasotun <servrostov@gmail.com>
2022-02-22 00:35:19 +0300

    add Реализовал функцию табы

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,43 @@ const tabsContainer = document.querySelector('.portfolio__tabs-wrapper');
 
 // Выбираем табы в контейнере
 const portfolioTabs = tabsContainer.querySelectorAll('.tab');
-console.log(portfolioTabs);
 
-// Выбираем картинки из портфолио по клику
+// Выбираем контейнер с картинками
+const imagesContainer = document.querySelector('.portfolio__gallery-wrapper');
+const portfolioImages = imagesContainer.querySelectorAll('.portfolio__image');
 
+// Скрываем все картинки на странице
+const hideAllImages = function () {
+  portfolioImages.forEach(image => {
+    image.classList.add('portfolio__image_hidden');
+  });
+};
+
+// Скрываем все картинки на странице
+const showAllImages = function () {
+  portfolioImages.forEach(image => {
+    image.classList.remove('portfolio__image_hidden');
+  });
+};
+
+// Показ изображений по тегу
 const selectImagesFromPortfolio = function () {
   tabsContainer.addEventListener('click', (evt) => {
     if (evt.target.classList.contains('tab')) {
       let clickedTag = evt.target;
-      removeActiveTabs();
-      togglePortfolioTag(clickedTag);
+      removeActiveTabs(); // Убираем со всех табов активный
+      togglePortfolioTag(clickedTag); // Меняем вид таба
+      hideAllImages(); // Скрываем все картинки
+      portfolioImages.forEach(image => {
+        if (image.dataset.name === clickedTag.innerText) {
+          image.classList.remove('portfolio__image_hidden');
+        } else if (clickedTag.innerText === 'All') {
+          showAllImages();
+        }
+      });
     }
   });
 };
-
 selectImagesFromPortfolio();
 
 // Для смены класса у кликнутого таба
