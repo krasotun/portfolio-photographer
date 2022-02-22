ba97349887a9806dd2a4eda15bcdd2d81f738fd8
Marat <servrostov@gmail.com>
2022-02-22 14:00:18 +0300

    add Реализовал добавление карточки из массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,23 @@
 // Работа с табами
+// Выбираем контейнер с картинками
+const imagesContainer = document.querySelector('.portfolio__gallery-wrapper');
+// Создаем картинку
+const portfolioImage = document.createElement('img');
+
+// Наполняем картинку
+const createPortfolioImage = function (name, link, alt) {
+  portfolioImage.dataset.name = name;
+  portfolioImage.src = link;
+  portfolioImage.alt = alt;
+  portfolioImage.classList.add('portfolio__image');
+};
+
+// Вставляем картинку на сайт
+const appendPortfolioImage = function () {
+  imagesContainer.prepend(portfolioImage);
+};
+createPortfolioImage(imagesArray[0].name, imagesArray[0].link, imagesArray[0].alt);
+appendPortfolioImage();
 
 // Выбираем контейнер с табами
 const tabsContainer = document.querySelector('.portfolio__tabs-wrapper');
@@ -6,9 +25,9 @@ const tabsContainer = document.querySelector('.portfolio__tabs-wrapper');
 // Выбираем табы в контейнере
 const portfolioTabs = tabsContainer.querySelectorAll('.tab');
 
-// Выбираем контейнер с картинками
-const imagesContainer = document.querySelector('.portfolio__gallery-wrapper');
+// Выбираем картинки из контейнера
 const portfolioImages = imagesContainer.querySelectorAll('.portfolio__image');
+
 
 // Скрываем все картинки на странице
 const hideAllImages = function () {
@@ -17,12 +36,13 @@ const hideAllImages = function () {
   });
 };
 
-// Скрываем все картинки на странице
+// Показываем все картинки на странице
 const showAllImages = function () {
   portfolioImages.forEach(image => {
     image.classList.remove('portfolio__image_hidden');
   });
 };
+
 
 // Показ изображений по тегу
 const selectImagesFromPortfolio = function () {
